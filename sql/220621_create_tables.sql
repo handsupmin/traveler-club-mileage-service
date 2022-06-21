949e2bcbfedeaab175b01fb01d3736a0b823f11a
handsupmin <thstkdals0318@gmail.com>
2022-06-21 22:31:42 +0900

    DDL 추가

---
+++ sql/220621_create_tables.sql
@@ -0,0 +1,33 @@
+CREATE SCHEMA `pointravel` DEFAULT CHARACTER SET utf8 ;
+
+CREATE TABLE `pointravel`.`user` (
+  `user_id` varchar(45) NOT NULL,
+  `point` int unsigned NOT NULL DEFAULT '0',
+  PRIMARY KEY (`user_id`));
+
+CREATE TABLE `pointravel`.`review` (
+  `review_id` VARCHAR(36) NOT NULL,
+  `user_id` VARCHAR(36) NOT NULL,
+  `place_id` VARCHAR(36) NOT NULL,
+  `content` VARCHAR(200) NULL,
+  PRIMARY KEY (`review_id`));
+
+CREATE TABLE `pointravel`.`place` (
+  `place_id` VARCHAR(36) NOT NULL,
+  `first_review_id` VARCHAR(36) NULL,
+  PRIMARY KEY (`place_id`));
+
+CREATE TABLE `pointravel`.`photo` (
+  `photo_id` varchar(36) NOT NULL,
+  `review_id` varchar(45) NOT NULL,
+  `user_id` varchar(45) NOT NULL,
+  PRIMARY KEY (`photo_id`));
+
+CREATE TABLE `pointravel`.`point_log` (
+  `log_id` INT NOT NULL AUTO_INCREMENT,
+  `review_id` VARCHAR(36) NOT NULL,
+  `user_id` VARCHAR(36) NOT NULL,
+  `point` INT NULL,
+  `bonus_point` INT NULL,
+  `total_point` INT NOT NULL,
+  PRIMARY KEY (`log_id`));

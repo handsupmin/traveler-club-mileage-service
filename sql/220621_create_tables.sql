17b657a6db0e1d329bc00031441280cd284f2afe
handsupmin <thstkdals0318@gmail.com>
2022-06-23 20:49:39 +0900

    테이블 변경 및 index 설정

--- sql/220621_create_tables.sql
+++ sql/220621_create_tables.sql
@@ -10,7 +10,8 @@ CREATE TABLE `pointravel`.`review` (
   `user_id` VARCHAR(36) NOT NULL,
   `place_id` VARCHAR(36) NOT NULL,
   `content` VARCHAR(200) NULL,
-  PRIMARY KEY (`review_id`));
+  PRIMARY KEY (`review_id`),
+  KEY `review_user_id_place_id` (`user_id`,`place_id`));
 
 CREATE TABLE `pointravel`.`place` (
   `place_id` VARCHAR(36) NOT NULL,
@@ -18,11 +19,13 @@ CREATE TABLE `pointravel`.`place` (
   PRIMARY KEY (`place_id`));
 
 CREATE TABLE `pointravel`.`photo` (
-  `photo_index` INT NOT NULL AUTO_INCREMENT,
+  `photo_index` int NOT NULL AUTO_INCREMENT,
   `photo_id` varchar(36) NOT NULL,
   `place_id` varchar(36) NOT NULL,
   `user_id` varchar(36) NOT NULL,
-  PRIMARY KEY (`photo_index`));
+  `review_id` varchar(36) NOT NULL,
+  PRIMARY KEY (`photo_index`),
+  KEY `photo_review_id` (`review_id`));
 
 CREATE TABLE `pointravel`.`point_log` (
   `log_id` INT NOT NULL AUTO_INCREMENT,
@@ -33,4 +36,5 @@ CREATE TABLE `pointravel`.`point_log` (
   `bonus_point` INT NULL,
   `total_point` INT NOT NULL,
   `write_date` datetime DEFAULT NULL,
-  PRIMARY KEY (`log_id`));
+  PRIMARY KEY (`log_id`),
+  KEY `point_log_user_id` (`user_id`));
